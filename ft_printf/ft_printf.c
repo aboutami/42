668d9c8e49b7b1b201bd981976419d8a300c80ef
Anass Boutami <aboutami@cbr1s1.42barcelona.com>
2025-01-20 21:48:04 +0100

    Mandatory

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboutami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 08:11:09 by aboutami          #+#    #+#             */
+/*   Updated: 2024/12/15 11:11:21 by aboutami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_switcher(char s, va_list *args, int *len, int *i)
+{
+	if (s == 's')
+		ft_string(va_arg(*args, char *), len);
+	else if (s == 'd' || s == 'i')
+		ft_number(va_arg(*args, int), len);
+	else if (s == 'u')
+		ft_unsigned_int(va_arg(*args, unsigned int), len);
+	else if (s == 'x')
+		ft_hexadecimal(va_arg(*args, unsigned int), len, 'x');
+	else if (s == 'X')
+		ft_hexadecimal(va_arg(*args, unsigned int), len, 'X');
+	else if (s == 'p')
+		ft_pointer(va_arg(*args, size_t), len);
+	else if (s == 'c')
+		ft_putchar(va_arg(*args, int), len);
+	else if (s == '%')
+		ft_putchar('%', len);
+	else
+		(*i)--;
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		length;
+
+	i = 0;
+	length = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			ft_switcher(format[i], &args, &length, &i);
+			i++;
+		}
+		else
+		{
+			ft_putchar((char)format[i], &length);
+			i++;
+		}
+	}
+	va_end(args);
+	return (length);
+}
---
+++ ft_printf/ft_words.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_words.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboutami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 08:12:03 by aboutami          #+#    #+#             */
+/*   Updated: 2024/12/12 09:32:13 by aboutami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putchar(char character, int *length)
+{
+	write(1, &character, 1);
+	(*length)++;
+}
+
+void	ft_string(char *args, int *length)
+{
+	size_t	i;
+
+	i = 0;
+	if (!args)
+	{
+		write(1, "(null)", 6);
+		(*length) += 6;
+		return ;
+	}
+	while (args[i] != '\0')
+	{
+		ft_putchar(args[i], length);
+		i++;
+	}
+}
